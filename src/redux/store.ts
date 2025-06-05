b71ebcf599ed4d68a93b91b5888c9180ec44fe5f
Think Dev <hocine.aachiq@gmail.com>
2025-06-05 09:05:37 +0100

    add redux-persist for favorites

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import favoritesReducer from "./favoriteSlice";
 
-export const store = configureStore({
-  reducer: {
-    favoriteSlice: favoritesReducer,
-  },
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  favoriteSlice: favoritesReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["favoriteSlice"], // ["favoriteSlice", "authSlice"]
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
